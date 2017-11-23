952a09ea9e1955a18d31a74d29641caa67ed4953
丁小华 <dingxiaohua@dingxiaohuadeMacBook-Pro.local>
2017-11-23 16:22:00 +0800

    add tcpclient

---
+++ net/tcpclient.cpp
@@ -0,0 +1,179 @@
+#include "tcpclient.h"
+
+#include "connector.h"
+#include "eventloop.h"
+#include "socketsops.h"
+#include "../base/log.h"
+
+#include <boost/bind.hpp>
+
+#include <stdio.h> // snprintf
+
+using namespace kaycc;
+using namespace kaycc::net;
+
+namespace kaycc {
+namespace net {
+namespace detail {
+
+    // 移除连接
+    void removeConnection(EventLoop* loop, const TcpConnectionPtr& conn) {
+        loop->queueInLoop(boost::bind(&TcpConnection::connectDestroyed, conn));
+    }
+
+    void removeConnector(const ConnectorPtr& connector) {
+        //connector->
+    }
+
+} //end detail
+} //end net
+}
+
+
+TcpClient::TcpClient(EventLoop* loop,
+                  const InetAddress& serverAddr,
+                  const std::string& name)
+    : loop_(loop),
+      connector_(new Connector(loop, serverAddr)),
+      name_(name),
+      connectionCallback_(defaultConnectionCallback),
+      messageCallback_(defaultMessageCallback),
+      retry_(false),
+      connect_(true),
+      nextConnId_(1) {
+
+    //一旦连接建立连接，回调newConnection
+    connector_->setNewConnectionCallback(
+        boost::bind(&TcpClient::newConnection, this, _1));
+
+    LOG << "TcpClient::TcpClient[" << name_
+        << "] - connector " << get_pointer(connector_) << std::endl;
+
+}
+
+TcpClient::~TcpClient() {
+    LOG << "TcpClient::~TcpClient[" << name_
+        << "] - connector " << get_pointer(connector_) << std::endl;
+
+    TcpConnectionPtr conn;
+    bool unique = false;
+
+    {
+        MutexLockGuard lock(mutex_);
+        unique = connection_.unique();
+        conn = connection_;
+    }
+
+    if (conn) {
+        assert(loop_ == conn->getLoop());
+        // FIXME: not 100% safe, if we are in different thread
+        // 移除TcpConnection对象
+        CloseCallback cb = boost::bind(&detail::removeConnection, loop_, _1);
+
+        // 调用连接关闭回调函数
+        loop_->runInLoop(
+            boost::bind(&TcpConnection::setCloseCallback, conn, cb));
+
+        if (unique) {
+            // 强制关闭
+            conn->forceClose();
+        }
+
+    } else {
+        // 停止连接器 
+        connector_->stop();
+
+        // 移除连接器 
+        loop_->runAfter(1, 
+            boost::bind(&detail::removeConnector, connector_));
+    }
+}
+
+void TcpClient::connect() {
+    LOG << "TcpClient::connect[" << name_ << "] - connecting to "
+        << connector_->serverAddress().toIpPort() << std::endl;
+
+    connect_ = true;
+    connector_->start();
+}
+
+void TcpClient::disconnect() {
+    connect_ = false;
+
+    {
+        MutexLockGuard lock(mutex_);
+        if (connection_) {
+            connection_->shutdown();
+        }
+
+    }
+}
+
+void TcpClient::stop() {
+    connect_ = false;
+    connector_->stop();
+}
+/*
+该函数创建一个堆上局部TcpConnection对象，并用TcpClient的智能指针connection_保存起来，这样本函数中conn即便析构掉，connection_依然维护该连接。
+然后设置各种回调函数。由于为了避免busy loop，在Connector中一旦连接成功，我们取消关注sockfd的可写事件。并且本函数使用conn->connectEstablished()内部会关注可读事件
+*/
+// 连接建立完毕会调用这个函数
+void TcpClient::newConnection(int sockfd) {
+    loop_->assertInLoopThread();
+    InetAddress peerAddr(sockets::getPeerAddr(sockfd));
+
+    char buf[32];
+    snprintf(buf, sizeof(buf), ":%s#%d", peerAddr.toIpPort().c_str(), nextConnId_);
+    ++nextConnId_;
+
+    std::string connName = name_ + buf;
+
+    InetAddress localAddr(sockets::getLocalAddr(sockfd));
+
+    // FIXME poll with zero timeout to double confirm the new connection  
+    // FIXME use make_shared if necessary  
+    //创建一个TcpConnection对象，智能指针。  
+    //根据Connector中的handleWrite()函数，连接建立后会把sockfd从poller中移除，以后不会再关注可写事件了  
+    //否则会出现busy loop，因为已连接套接字一直处于可写状态  
+    TcpConnectionPtr conn(new TcpConnection(loop_,
+                                            connName,
+                                            sockfd,
+                                            localAddr,
+                                            peerAddr));
+
+    conn->setConnectionCallback(connectionCallback_);
+    conn->setMessageCallback(messageCallback_);
+    conn->setWriteCompleteCallback(writeCompleteCallback_);
+    conn->setCloseCallback(
+        boost::bind(&TcpClient::removeConnection, this, _1)); // FIXME: unsafe
+
+    {
+        MutexLockGuard lock(mutex_);
+        connection_ = conn;
+    }
+
+    conn->connectEstablished();
+}
+
+// 移除一个链接
+void TcpClient::removeConnection(const TcpConnectionPtr& conn) {
+    loop_->assertInLoopThread();
+    assert(loop_ == conn->getLoop());
+
+    {
+        MutexLockGuard lock(mutex_);
+        assert(connection_ == conn);
+        connection_.reset(); //重置 
+    }
+
+      //I/O线程中销毁
+    loop_->queueInLoop(boost::bind(&TcpConnection::connectDestroyed, conn));
+
+    if (retry_ && connect_) {
+        LOG << "TcpClient::connect[" << name_ << "] - Reconnecting to "
+            << connector_->serverAddress().toIpPort() << std::endl;
+
+        //这里的重连是连接成功后断开的重连，所以实际上是重启 
+        connector_->restart();
+    }
+}
---
+++ net/tcpclient.h
@@ -0,0 +1,96 @@
+#ifndef KAYCC_NET_TCPCLIENT_H 
+#define KAYCC_NET_TCPCLIENT_H
+
+#include <boost/noncopyable.hpp>
+
+#include "../base/mutex.h"
+#include "tcpconnection.h" 
+
+namespace kaycc {
+namespace net {
+
+    class Connector;
+    typedef boost::shared_ptr<Connector> ConnectorPtr;
+
+    class TcpClient : boost::noncopyable {
+    public:
+        TcpClient(EventLoop* loop,
+                  const InetAddress& serverAddr,
+                  const std::string& name);
+
+        ~TcpClient(); // force out-line dtor, for scoped_ptr members.
+
+        void connect();
+        void disconnect();
+        void stop();
+
+        TcpConnectionPtr connection() const {
+            MutexLockGuard lock(mutex_);
+            return connection_;
+        }
+
+        EventLoop* getLoop() const {
+            return loop_;
+        }
+
+        bool retry() const {
+            return retry_;
+        }
+
+        void enableRetry() {
+            retry_ = true;
+        }
+ 
+        const std::string& name() const {
+            return name_;
+        }
+
+        /// Set connection callback.
+        /// Not thread safe.
+        void setConnectionCallback(const ConnectionCallback& cb) {
+            connectionCallback_ = cb;
+        }
+
+        /// Set message callback.
+        /// Not thread safe.
+        void setMessageCallback(const MessageCallback& cb) {
+            messageCallback_ = cb;
+        }
+
+        /// Set write complete callback.
+        /// Not thread safe.
+        void setWriteCompleteCallback(const WriteCompleteCallback& cb) {
+            writeCompleteCallback_ = cb;
+        }
+
+    private:
+        // 连接建立完毕会调用这个函数
+        void newConnection(int sockfd);
+
+        // 移除一个链接
+        void removeConnection(const TcpConnectionPtr& conn);
+
+        // 所属的Reactor  
+        EventLoop* loop_;
+        ConnectorPtr connector_; // avoid revealing Connector
+
+        const std::string name_;
+        ConnectionCallback connectionCallback_;
+        MessageCallback messageCallback_;
+        WriteCompleteCallback writeCompleteCallback_;
+
+        bool retry_; // atomic   //是否重连，是指建立的连接成功后又断开是否重连。
+        bool connect_; // atomic
+
+        // always in loop thread
+        int nextConnId_; //name_+nextConnid_用于标识一个连接
+
+        mutable MutexLock mutex_;
+        TcpConnectionPtr connection_;  //Connector连接成功后，得到一个TcpConnection
+    };
+
+
+} //end net
+}
+
+#endif
